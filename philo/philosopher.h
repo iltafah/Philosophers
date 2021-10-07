bbc191a25e01b29c4289a7ea817879e20ec688ae
Ilias Tafah <iltafah@e1r7p14.1337.ma>
2021-10-07 16:50:40 +0100

    just pushing unfinished work

--- philo/philosopher.h
+++ philo/philosopher.h
@@ -6,7 +6,7 @@
 /*   By: iltafah <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 18:31:10 by iltafah           #+#    #+#             */
-/*   Updated: 2021/10/06 17:13:51 by iltafah          ###   ########.fr       */
+/*   Updated: 2021/10/07 15:49:41 by iltafah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <pthread.h>
+# include <sys/time.h>
 
 # define NONE 0
 # define ERROR -1
@@ -26,13 +27,32 @@
 # define MAX_INT 2147483647
 # define MIN_INT -2147483648
 
+enum e_bool {false, true};
+
+typedef enum e_is_available {not_available, available} t_is_available;
+
 typedef struct s_philo_vars
 {
-	int		time_to_die;
-	int		time_to_eat;
-	int		num_of_philos;
-	int		time_to_sleep;
-	int		eating_repeat_time;
+	int				time_to_die;
+	int				time_to_eat;
+	int				num_of_philos;
+	int				time_to_sleep;
+	int				eating_repeat_time;
+	int				simulation_starting_time;
+	// t_is_available	*forks_availability;
+	pthread_mutex_t	*forks_mutex;
 }				t_philo_vars;
+
+typedef struct s_philosophers
+{
+	int				id;
+	int				is_eating;
+	int				is_sleeping;
+	int				is_thinking;
+	t_is_available	is_fork_available;
+	t_is_available	*next_fork_ptr;
+}				t_philosophers;
+
+static t_philo_vars	philo_vars;
 
 #endif
--- philo/philosopher_main.c
+++ philo/philosopher_main.c
@@ -6,15 +6,42 @@
 /*   By: iltafah <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 18:31:01 by iltafah           #+#    #+#             */
-/*   Updated: 2021/10/06 17:14:58 by iltafah          ###   ########.fr       */
+/*   Updated: 2021/10/07 16:49:51 by iltafah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./philosopher.h"
 
-void	simulation()
+void	*simulation(void *given_philo)
 {
+	struct timeval	time_struct;
+	int				repated_times;
+	t_philosophers	curr_philo;
 	
+	repated_times = 0;
+	curr_philo = *(t_philosophers *)given_philo;
+	while (repated_times < philo_vars.eating_repeat_time)
+	{
+		gettimeofday(&time_struct, NULL);
+		if (curr_philo.is_fork_available == available)
+		{
+			pthread_mutex_lock(&philo_vars.forks_mutex[curr_philo.id - 1]);
+			printf("%ld %d has taken a fork\n", ((time_struct.tv_sec + (time_struct.tv_usec / 1000000))/1000 - philo_vars.simulation_starting_time), curr_philo.id);
+			curr_philo.is_fork_available = not_available;
+			pthread_mutex_lock(&philo_vars.forks_mutex[curr_philo.id %  philo_vars.num_of_philos]);
+			*(curr_philo.next_fork_ptr) = not_available;
+			printf("%ld %d has taken a fork\n", ((time_struct.tv_sec + (time_struct.tv_usec / 1000000))/1000 - philo_vars.simulation_starting_time), curr_philo.id);
+		}
+		printf("%ld %d is eating\n", ((time_struct.tv_sec + (time_struct.tv_usec / 1000000))/1000 - philo_vars.simulation_starting_time), curr_philo.id);
+		usleep(philo_vars.time_to_eat * 1000);
+
+		pthread_mutex_unlock(&philo_vars.forks_mutex[curr_philo.id - 1]);
+		pthread_mutex_unlock(&philo_vars.forks_mutex[curr_philo.id %  philo_vars.num_of_philos]);
+		gettimeofday(&time_struct, NULL);
+		printf("%ld %d is sleeping\n", ((time_struct.tv_sec + (time_struct.tv_usec / 1000000))/1000 - philo_vars.simulation_starting_time), curr_philo.id);
+		repated_times++;
+	}
+	return (NULL);
 }
 
 int	convert_arg_to_int(char *argv, int *error)
@@ -44,7 +71,8 @@ int	convert_arg_to_int(char *argv, int *error)
 
 int	initialize_philo_vars(t_philo_vars *philo_vars, char **argv)
 {
-	int		error;
+	int				error;
+	struct timeval	time_struct;
 
 	error = NONE;
 	philo_vars->num_of_philos = convert_arg_to_int(argv[0], &error);
@@ -57,13 +85,22 @@ int	initialize_philo_vars(t_philo_vars *philo_vars, char **argv)
 		philo_vars->eating_repeat_time = 0;
 	if (error == ERROR)
 		return (ERROR);
+	// philo_vars->forks_availability = malloc(sizeof(t_is_available) * philo_vars->num_of_philos);
+	philo_vars->forks_mutex = malloc(sizeof(pthread_mutex_t) * philo_vars->num_of_philos);
+	for (int i = 0; i < philo_vars->num_of_philos; i++)
+	{
+		pthread_mutex_init(&philo_vars->forks_mutex[i], NULL);
+		// philo_vars->forks_availability[i] = available;
+	}
+	gettimeofday(&time_struct, NULL);
+	philo_vars->simulation_starting_time = (time_struct.tv_sec + (time_struct.tv_usec / 1000000)) / 1000;
 	return (SUCCESS);
 }
 
 int main(int argc, char **argv)
 {
-	t_philo_vars	philo_vars;
-	int				return_code;
+	t_philosophers	*philo_simulation;
+	int					return_code;
 
     if (argc == MAX_ARGS_NUM || argc == MAX_ARGS_NUM + OPTIONAL_ARG)
     {
@@ -73,6 +110,41 @@ int main(int argc, char **argv)
 			printf("Error\n");
 			return (0);
 		}
+		//////////
+		pthread_t *thread_id = malloc(sizeof(pthread_t) * 5);
+		int			*philo_ids = malloc(sizeof(int) * 5);
+		for (int id = 0; id < philo_vars.num_of_philos; id++)
+			philo_ids[id] = id + 1;
+
+		philo_simulation = malloc(sizeof(t_philosophers) * philo_vars.num_of_philos);
+
+		int i = 0;
+		while (i < philo_vars.num_of_philos)
+		{
+			philo_simulation[i].id = i + 1;
+			philo_simulation[i].is_eating = false;
+			philo_simulation[i].is_sleeping = false;
+			philo_simulation[i].is_thinking = false;
+			philo_simulation[i].is_fork_available = available;
+			philo_simulation[i].next_fork_ptr = &philo_simulation[(i + 1) % philo_vars.num_of_philos].is_fork_available;
+			i++;
+		}
+		
+		
+		i = 0;
+		while (i < philo_vars.num_of_philos)
+		{
+			pthread_create(&thread_id[i], NULL, simulation, (void *)&philo_simulation[i]);
+			pthread_detach(thread_id[i]);
+			usleep(50);
+			i++;
+		}
+
+while (1)
+{
+	
+}
+
     }
 	else
 		printf("Error\n");
